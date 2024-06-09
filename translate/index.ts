ff8d12c45c3bc429353271909fee63ba5c3353d1
KillerCoder <killer-coder@hotmail.com>
2024-06-09 16:56:04 +0100

    remove translations from the bot

--- translate/index.ts
+++ translate/index.ts
@@ -6,16 +6,10 @@ setBulk({
         'auth.loading.discord': 'Validating discord account',
         'auth.loading.failed': 'We found a problem validating the account',
         'auth.register.failed': 'We found a problem creating the account',
-        'dc.systems.ok': 'All systems OK! 🌍',
-        'dc.title': 'MetaRP \t\t 🚀',
-        'dc.version': 'version: 1.0.0'
     },
     pt: {
         'auth.loading.discord': 'A validar conta discord',
         'auth.loading.failed': 'Houve um problema ao validar a conta',
         'auth.register.failed': 'Houve um problema ao criar a conta',
-        'dc.systems.ok': 'Todos os serviços estão operacionais! 🌍',
-        'dc.title': 'MetaRP \t\t 🚀',
-        'dc.version': 'versão: 1.0.0'
     }
 });
